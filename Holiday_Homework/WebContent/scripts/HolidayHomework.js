50b1dc2deb41215a67a63c305665958b62897162
mbogdanoff <mbogdanov@mail.com>
2014-12-29 18:40:00 +0200

    Update HolidayHomework.js

--- Holiday_Homework/WebContent/scripts/HolidayHomework.js
+++ Holiday_Homework/WebContent/scripts/HolidayHomework.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
 	"use strict"
-	// Task 1
-	console.log($(".tu").attr("title"));
 	// Task 2
+	console.log($(".tu").attr("title"));
+	// Task 3
 	console.log($("#col1 p").text());
+	// Task 4
+	var $element = $("<li id='menu-item-last' > <a href=>new button</a></li>");
+	var list = $("ul#menu-top-level-menu");
+	list.append($element);
 });
