8d290beb39a85c003dc4e22865cf988d5af2c420
mbogdanoff <mbogdanov@mail.com>
2015-02-19 17:14:31 +0200

    Event propagation and event catching example

--- 06_SampleComponents/WebContent/scripts/dismissAlert.js
+++ 06_SampleComponents/WebContent/scripts/dismissAlert.js
@@ -1,18 +1,20 @@
-$(document).ready(function() {
-	"use strict"
-	// events are propagated from selected element
-	// up to the document (through all parents)
-	// we catch the event on the first parent - document
-	$(document).on("click",
+$(document).ready(function () {
+    "use strict"
+    // events are propagated from selected element
+    // up to the document (through all parents)
+    // we catch the event on the first parent - document
+    $(document).on("click",
 		// we filter only click events on elements
 		// that have attribute data-dismiss-sample
 		// with value 'alert'
-		"[data-dismiss-sample='alert']", function(event) {
-		// you can use this instead of event.target
-		$(event.target.closest(".alert")).slideUp("slow", function() {
-			$(this).remove();
+		"[data-dismiss-sample='alert']", function () {
+		    console.log("dismiss alert", arguments);
 		});
-		// see bootstrap implementation: https://github.com/twbs/bootstrap/blob/master/js/alert.js
-	});
-	
+
+    // all click events on the page
+    // (except events that are not propagated)
+    $(document).on("click", function () {
+        console.log("all", arguments);
+    });
+
 });
